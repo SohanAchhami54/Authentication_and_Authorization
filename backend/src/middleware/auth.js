230c105ef5b2e8d21220c7e6022302c890770033
Sohan Ac <sohanachhami655@gmail.com>
2026-01-29 15:34:12 +0545

    feat(middleware): add JWT-based protected route

---
+++ backend/src/middleware/auth.js
@@ -0,0 +1,19 @@
+import { findUser, verifyToken } from "../utils/auth.js";
+import { AsyncError } from "../utils/catchAsyncError.js";
+import { Errorhandler } from "./error.middleware.js";
+
+const isProtected=AsyncError(async(req,res,next)=>{
+    const {token}=req.cookies 
+    if(!token) return next(new Errorhandler('User is not authenticated',400))
+    
+    const decoded=verifyToken(token)
+    console.log('decoded value is:',decoded)
+    if(!decoded) return next(new Errorhandler('Unauthorized Token',400))
+    
+    const userData=await findUser(decoded.id)
+    console.log('userdata is:',userData)
+    res.user=userData
+    next()
+})
+
+export {isProtected}
