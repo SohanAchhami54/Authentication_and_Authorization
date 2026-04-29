1cbfa9a49eb18fb43e970c8c3c9aea4c3414d4e0
Sohan Ac <sohanachhami655@gmail.com>
2026-04-29 17:35:07 +0545

    feat(api): implement user lgin and registration api calls

--- frontend/src/services/auth.js
+++ frontend/src/services/auth.js
@@ -1,15 +1,31 @@
 import { toast } from "react-toastify"
 import Axios from "../api/axios"
 
-const userLogin=async(formData)=>{
+const userLogin=async(formData,setUser,setIsAuthenticated)=>{
      try{
           const {data}= await Axios.post('/api/auth/signin',formData)
           if(data.success){
              localStorage.setItem('token',data.data.token) 
               toast.success(data.message)
+              setIsAuthenticated(true)
+              setUser(data.data)
           }
         }catch(error){
            toast.error(error.response?.data?.message)
         }
 }
-export {userLogin}
+
+const userRegister=async(formData,navigate)=>{
+   try{
+       const {data}=await Axios.post('/api/auth/signup',formData)
+        navigate(`/otp/${formData.email}/${formData.phone}`)
+      //  if(data.success){
+      //    toast.success('User Register Successfully')
+      //  }
+   }catch(error){
+      toast.error(error.response?.data?.message)
+   }
+}
+
+
+export {userLogin,userRegister}
