842d9917adb789c5493bafc1aa353360c7603d7d
Sohan Ac <sohanachhami655@gmail.com>
2026-05-01 20:34:37 +0545

    feat(password): add the main logic behind forgot and reset password

--- frontend/src/services/auth.js
+++ frontend/src/services/auth.js
@@ -9,6 +9,7 @@ const userLogin=async(formData,setUser,setIsAuthenticated)=>{
               toast.success(data.message)
               setIsAuthenticated(true)
               setUser(data.data)
+              navigate('/')
           }
         }catch(error){
            toast.error(error.response?.data?.message)
@@ -19,13 +20,52 @@ const userRegister=async(formData,navigate)=>{
    try{
        const {data}=await Axios.post('/api/auth/signup',formData)
         navigate(`/otp/${formData.email}/${formData.phone}`)
-      //  if(data.success){
-      //    toast.success('User Register Successfully')
-      //  }
+      
+       if(data.success){
+         toast.success('OTP send to your email')
+       }
    }catch(error){
       toast.error(error.response?.data?.message)
    }
 }
 
+const handleOtp=async(payload,setUser,setIsAuthenticated,navigate)=>{
+   try{
+       const {data}=await Axios.post('/api/auth/otpverification',payload)
+       if(data.success){
+         toast.success(data.message) 
+         setIsAuthenticated(true) 
+         navigate('/login')
 
-export {userLogin,userRegister}
+       }
+   }catch(error){
+      toast.error(error.response?.data?.message)
+      setIsAuthenticated(null)
+   }
+}
+
+const  manangeForgotPass=async(email,reset)=>{
+  try{
+    const {data}=await Axios.post('/api/auth/reset/forgotpassword',{email}) 
+    if(data.success){
+      toast.success(data.message)
+      reset()
+    }
+  }catch(error){
+     toast.error(error.response?.data?.message)
+  }
+}
+
+const manangeResetPass=async(password,confirmPassword,token,navigate)=>{
+   try{
+       const {data} = await Axios.put(`/api/auth/reset/resetpassword/${token}`,{password,confirmPassword})
+       if(data.success){
+         toast.success(data.message)
+         navigate('/')
+       }
+   }catch(error){
+      toast.error(error.response?.data?.message)
+   }
+}
+
+export {userLogin,userRegister,handleOtp, manangeForgotPass,manangeResetPass}
