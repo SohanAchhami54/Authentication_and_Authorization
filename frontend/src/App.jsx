0dad06809efbaeb4ad2b81de8bd69fab7a454e43
Sohan Ac <sohanachhami655@gmail.com>
2026-05-03 21:09:14 +0545

    fix(route): correct the routing logic by adding protected route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Forgotpass from './pages/Forgotpass'
 import Auth from './pages/Auth'
@@ -9,19 +9,36 @@ import Layout from './layout/Layout'
  import { ToastContainer, toast } from 'react-toastify';
 import Login from './components/Login'
 import Register from './components/Register'
+import { useApp } from './context/context'
+import NotFound from './pages/NotFound'
 const App = () => {
+  const {user,loading}=useApp()
+  if(loading) return <div>Loading app....</div>
   return (
     <>
     <ToastContainer/>
     <Routes>
     <Route path='/' element={<Layout/>}>
        <Route index element={<Home/>}/>
-       <Route path='/auth' element={<Auth/>}/>
-       <Route path='/otp' element={<Otpverified/>}/>
-       <Route path='/password/forgot' element={<Forgotpass/>}/>
+       <Route path='/otp/:email/:phone' element={<Otpverified/>}/>
+    
        <Route path='/password/reset/:token' element={<Resetpass/>}/>
-       <Route path='/login' element={<Login/>} />
-       <Route path='/register' element={<Register/>} />
+       {!user && (
+        <>
+         <Route path='/login' element={<Login/>} />
+         <Route path='/register' element={<Register/>} />
+         <Route path='/password/forgot' element={<Forgotpass/>}/>
+        </>
+       )}
+       {user && (
+        <>
+         <Route path='/login' element={<Navigate to='/' replace/>} />
+         <Route path='/register' element={<Navigate to='/' replace />} />
+        </>
+       )}
+       <Route path='*' element={<NotFound/>}></Route>
+       
+      
     </Route>
    </Routes>
    </>
