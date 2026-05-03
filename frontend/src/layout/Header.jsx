d0489f7301f43bcfc7a982ac2974a5e18bb53f10
Sohan Ac <sohanachhami655@gmail.com>
2026-05-03 21:15:44 +0545

    feat(auth): add logout logic

--- frontend/src/layout/Header.jsx
+++ frontend/src/layout/Header.jsx
@@ -1,27 +1,36 @@
 import React, { useState } from 'react'
 import { NavLink } from 'react-router-dom'
 import { GiHamburgerMenu } from "react-icons/gi";
+import { useApp } from '../context/context';
 const Header = () => {
   const isNavActive=({isActive})=>{
     return isActive?'text-red-600':''
   }
   const [open,setOpen]=useState(false)
+  const {handleLogout,isAuthenticated}=useApp()
 
   return (
     <>
       <header className='relative flex items-center justify-between md:justify-around  bg-green-400 p-3 font-semibold '>
          <div>
-            <h2 className='text-xl'>Authentication</h2>
+            <h2 className='text-xl'><NavLink to='/'> Authentication</NavLink></h2>
          </div>
          {/* for larger screen  */}
          <div className=' hidden md:flex gap-10'>
-            <NavLink to='/' className={isNavActive}>Home</NavLink>
-            <NavLink to='/login' className={isNavActive}>Login</NavLink>
-            <NavLink to='/register' className={isNavActive}>Register</NavLink>
-            {/* <NavLink to='/auth' className={isNavActive}>Auth</NavLink>
-            <NavLink to='/otp' className={isNavActive}>OTP</NavLink>
-            <NavLink to='/password/forgot' className={isNavActive}>ForgotPassword</NavLink>
-            <NavLink to='/password/reset/:token' className={isNavActive}>ResetPassword</NavLink> */}
+
+           {
+                  isAuthenticated?(
+                     <>
+                       <button onClick={()=>{handleLogout()}} className='cursor-pointer'>Logout</button>
+                     </>
+                  ):(
+                     <>
+                        <NavLink to='/' className={isNavActive}  >Home</NavLink>
+                        <NavLink to='/login' className={isNavActive} >Login</NavLink>
+                       <NavLink to='/register' className={isNavActive} >Register</ NavLink>
+                     </>
+                  )
+               }
          </div>
             { !open && (
                <GiHamburgerMenu onClick={()=>setOpen(prev=>!prev)}   className='flex md:hidden' />
@@ -30,13 +39,19 @@ const Header = () => {
          { 
             open && (
              <nav className='flex flex-col gap-3 absolute top-0 right-0 p-2  min-h-screen bg-green-300 '>
-              <NavLink to='/' className={isNavActive} onClick={()=>setOpen(prev=>!prev)} >Home</NavLink>
-               <NavLink to='/login' className={isNavActive} onClick={()=>setOpen(prev=>!prev)}>Login</NavLink>
-              <NavLink to='/register' className={isNavActive} onClick={()=>setOpen(prev=>!prev)}>Register</NavLink>
-              {/* <NavLink to='/auth' className={isNavActive} onClick={()=>setOpen(prev=>!prev)}>Auth</NavLink>
-            <NavLink to='/otp' className={isNavActive} onClick={()=>setOpen(prev=>!prev)}>OTP</NavLink>
-            <NavLink to='/forgotpassword' className={isNavActive} onClick={()=>setOpen(prev=>!prev)}>ForgotPassword</NavLink>
-            <NavLink to='/resetpassword' className={isNavActive} onClick={()=>setOpen(prev=>!prev)}>ResetPassword</NavLink>  */}
+               {
+                  isAuthenticated?(
+                     <>
+                       <button onClick={()=>{handleLogout()}} className='cursor-pointer'>Logout</button>
+                     </>
+                  ):(
+                     <>
+                        <NavLink to='/' className={isNavActive} onClick={()=>setOpen(prev=>!prev)} >Home</NavLink>
+                       <NavLink to='/login' className={isNavActive} onClick={()=>setOpen(prev=>!prev)}>Login</NavLink>
+                       <  NavLink to='/register' className={isNavActive} onClick={()=>setOpen(prev=>!prev)}>Register</    NavLink>
+                     </>
+                  )
+               }
             </nav>
             )}
       </header>
