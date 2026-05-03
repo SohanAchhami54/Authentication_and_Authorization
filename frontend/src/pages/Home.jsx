f991cd467e6e3cc1c548fa93e8c11eaeb5eb18cb
Sohan Ac <sohanachhami655@gmail.com>
2026-05-03 21:13:28 +0545

    feat(home): add home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { useApp } from '../context/context'
 
 const Home = () => {
+  const { user } = useApp()
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="min-h-screen flex flex-col justify-center items-center bg-gray-100">
+      <div className="bg-white shadow-lg rounded-2xl p-8 text-center">
+        <h1 className="text-3xl font-bold text-gray-800 mb-4">
+          Home
+        </h1>
+
+        <span className="text-lg text-gray-600">
+          Hello, <span className="font-semibold text-blue-600">{user?.name}</span> Developer
+        </span>
+      </div>
     </div>
   )
 }
