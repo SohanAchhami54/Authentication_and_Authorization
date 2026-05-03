0b84e96509f1a73035f70e154ca7b369806f9526
Sohan Ac <sohanachhami655@gmail.com>
2026-05-03 21:12:29 +0545

    feat(auth): add login persistence and user data fetching

--- frontend/src/context/context.jsx
+++ frontend/src/context/context.jsx
@@ -1,12 +1,57 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
+import Axios from "../api/axios";
+import { toast } from "react-toastify";
 const AppContext=createContext() 
 
 const AppContextProvider=({children})=>{
 
-    const [isAuthenticated,setIsAuthenticated]=useState()
-    const [user,setUser]=useState()
+    const [isAuthenticated,setIsAuthenticated]=useState(false)
+    const [user,setUser]=useState('')
+    const [token,setToken]=useState('')
+    const [loading,setLoading]=useState(true)
+    
 
-    const value={isAuthenticated,setIsAuthenticated,user,setUser}
+ 
+    const getUserData=async()=>{
+        try{
+            const {data}=await Axios.get('/api/auth/getuser') 
+            if(data.success){
+            setUser(data.user)
+            setIsAuthenticated(true)
+           }
+         }catch(error){
+            setUser(null)
+        }finally{
+            setLoading(false)
+        }
+       
+    }
+
+       useEffect(()=>{
+        const token=localStorage.getItem('token') 
+        if(token){
+          setToken(token) 
+          getUserData()
+        }else{
+            setLoading(false)
+        }
+         
+      },[])
+ 
+    
+   
+
+    const handleLogout=()=>{
+        const confirmed=window.confirm('Are u Sure want to Logout') 
+        if(!confirmed) return  
+        localStorage.removeItem('token') 
+        setUser(null) 
+        setIsAuthenticated(false)
+        setToken('')
+        toast.success('You have been logout')
+    }
+
+    const value={isAuthenticated,setIsAuthenticated,user,setUser,handleLogout,loading}
     return (
         <AppContext.Provider value={value}>
            {children}
